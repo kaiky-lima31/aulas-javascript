42e78221f43ab59b9f24e06df8f95dc077b81cf6
kaiky-lima31 <kaiky.lima357@gmail.com>
2023-04-18 17:27:04 -0300

    Exercicio - desafio

---
+++ js/exercicio03.js
@@ -0,0 +1,9 @@
+console.log("Exercicio 03");
+
+/* Gerar valores entre 0 a 10 (sem casa deciaml) */
+let numeroAleatorio = parseInt(Math.random() * 11);
+console.log(numeroAleatorio);
+
+let numero = 1 & 10
+
+if(numero <)
