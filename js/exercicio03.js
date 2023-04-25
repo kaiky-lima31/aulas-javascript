167fb8612c31f2c7f614f6f72ae21cbf2ba43eb1
kaiky-lima31 <kaiky.lima357@gmail.com>
2023-04-25 15:32:41 -0300

    Entrnando mais em JS

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,4 +1,36 @@
-console.log("Exercicio 03");
+const formulario = document.querySelector("form");
+const campoPalpite = document.querySelector("#palpite");
+const resultado = document.querySelector("#resultado")
+
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let palpiteDigitado = parseInt(campoPalpite.value);
+    console.log(`Palpite: ${palpiteDigitado}`);
+
+        /* Se o palpite digitado pelo usuário 
+        for igual ao número aleatorio que o jogo gerou */
+    if(palpiteDigitado == numeroAleatorio){
+        // Então acertou!
+        resultado.innerHTML = "Acertou mirezavi!!! 😎";
+        resultado.style.color = "blue";
+    /*Senão, se o palpite digitado for menor quje zero 
+    OU maior que 10 */
+    } else if( palpiteDigitado < 0 || palpiteDigitado > 10 ){
+        // Então informar que os valores precisam ser entre 0 e 10
+        resultado.innerHTML = "Digite um valor entre 0  e 10! 😉";
+        resultado.style.color = "red";
+    } else {
+        // Senão, errou!
+        resultado.innerHTML = "Xiiii errouuuuuuu :) 😂";
+        resultado.style.color = "red";
+
+        imagens.src = faustao;
+    }
+
+
+});
 
 /* Gerar valores entre 0 a 10 (sem casa deciaml) */
 let numeroAleatorio = parseInt(Math.random() * 11);
@@ -6,4 +38,4 @@ console.log(numeroAleatorio);
 
 let numero = 1 & 10
 
-if(numero <)
+
